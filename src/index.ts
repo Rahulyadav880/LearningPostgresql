50fc4e242a428f8125bf87ec5bc20c6a0d8941b8
Rahul Yadav <ry9064526713@gmail.com>
2026-04-14 23:54:34 +0530

    ran my first command

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,21 @@
 import { Client } from "pg";
 
+//const pgClient = new Client("postgresql://neondb_owner:npg_yzAW36oDMxvm@ep-sparkling-sea-anzgw2z9-pooler.c-6.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require")
+
+//if this feels jarring, we can also use it like above
 const pgClient = new Client({
-    user : 
+    user : "neondb_owner",
+    password : "npg_yzAW36oDMxvm",
+    port : 5432,
+    host : "ep-sparkling-sea-anzgw2z9-pooler.c-6.us-east-1.aws.neon.tech",
+    database : "neondb",
+    ssl : true
 })
+
+async function main(){
+    await pgClient.connect();
+    const response = await pgClient.query("SELECT * FROM users");
+    console.log(response);
+}
+
+main();
