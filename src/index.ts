502d755177db2219443ae421cf37b097822ef71c
Rahul Yadav <ry9064526713@gmail.com>
2026-04-17 22:24:15 +0530

    learned sql injection and prevention

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,46 @@
+import express from "express";
 import { pgClient } from "./db.js";
 
+const app = express();
+app.use(express.json());
 
+pgClient.connect();
 
+app.post("/signup", async(req, res) => {
+    const username = req.body.username;
+    const email = req.body.email;
+    const password = req.body.password;
+    
+    console.log(req.body)
+    try{
+        
+    //these are called placeholders or parameters ($1, $2, $3)
+    const insertQuery = await pgClient.query(`INSERT INTO users (username, email, password) VALUES ($1, $2, $3)`, [username, email, password])
+    
+    res.json({
+        message : "You are signed up"
+    })
 
+    }catch(e){
+        console.log(e)
+        res.json({
+            message : "error while signing up"
+        })
+    }
+    
+})
  
-async function main(){
-    await pgClient.connect();
-    const response = await pgClient.query("SELECT * FROM users");
-    console.log(response.rows);
-}
+app.listen(3000);
 
-main();
+//SQL INJECTION
+//const insertQuery = await pgClient.query(`INSERT INTO users (username, email, password) VALUES (${username}, ${email}, ${password})`);
+//if we write the above syntax for the database query then anyone can inject sql
+//via let's say password something like
+//  {
+//       username : "Rahul",
+//       email : "rahul@gmail.com",
+//       password : "123123'); DELETE FROM users; INSERT INTO users (username, email, password) VALUES 
+//        ('123', '1234', '12345"
+//    }
+
+// this query will be injected through the loophole
